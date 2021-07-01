9506905a0e01e959c7967119a2a35bb089d26953
NgocSon15 <ngocson1562k@gmail.com>
2021-07-01 10:45:56 +0700

    web layout solution

---
+++ web_layout/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Web Layout</title>
+	<link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+	<div class="container">
+        <header><?php  include "header.php";  ?></header>
+        <nav><?php include "nav.php";  ?></nav>
+        <article> <?php include "content.php"; ?></article>
+        <footer><?php include "footer.php";?></footer>
+    </div>
+</body>
+</html>
