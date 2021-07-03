1179102b27857de80175aa3cfa1fa1973e853c83
NgocSon15 <ngocson1562k@gmail.com>
2021-07-03 22:03:46 +0700

    single pattern

---
+++ single_pattern/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php 
+		class Application {
+		    private static $instance;
+
+		    private function __construct()
+		    {
+		    }
+
+		    public static function getInstance() {
+		        if(self::$instance === null) {
+		            self::$instance = new Application();
+		            echo 'alo';
+		        }
+		        return self::$instance;
+		    }
+		}
+
+		$app1 = Application::getInstance();
+		$app2 = Application::getInstance();
+		$app3 = new Application(); //Error
+	 ?>
+</body>
+</html>
