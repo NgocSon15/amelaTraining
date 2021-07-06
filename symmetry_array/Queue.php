fb54c3f7b0d5703aa98d81445a138eb9d482cc54
NgocSon15 <ngocson1562k@gmail.com>
2021-07-06 11:39:48 +0700

    symmetry

---
+++ symmetry_array/Queue.php
@@ -0,0 +1,39 @@
+<?php 
+	include_once('Node.php');
+
+	class Queue
+	{
+		public $front;
+		public $back;
+
+		public function isEmpty()
+		{
+			return $this->front === null;
+		}
+
+		public function enqueue($value)
+		{
+			$oldBack = $this->back;
+			$this->back = new Node();
+			$this->back->value = $value;
+			if($this->isEmpty())
+			{
+				$this->front = $this->back;
+			} else {
+				$oldBack->next = $this->back;
+			}
+		}
+
+		public function dequeue()
+		{
+			if ($this->isEmpty())
+			{
+				return null;
+			} else {
+				$removedValue = $this->front->value;
+				$this->front = $this->front->next;
+				return $removedValue;
+			}
+		}
+	}
+?> 
